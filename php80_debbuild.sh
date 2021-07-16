4b2c295f327eb83525fe1c8794ceaaeeeb9b89ac
Wjinlei <1976883731@qq.com>
2021-07-16 10:10:43 +0800

    Fix

--- php80_debbuild.sh
+++ php80_debbuild.sh
@@ -717,20 +717,21 @@ _build_deb(){
     mkdir -p ${buildroot}${php80_location}/lib
     cd ${buildroot}${php80_location}/lib
     # Fix libtidy
-    cp -a /usr/lib/libtidys.a ${buildroot}${php80_location}/lib
-    cp -a /usr/lib/libtidy.so.5 ${buildroot}${php80_location}/lib
-    cp -a /usr/lib/libtidy.so.5.2.0 ${buildroot}${php80_location}/lib
+    cp -a /usr/lib/libtidy-0.99.so.0 ${buildroot}${php80_location}/lib
+    cp -a /usr/lib/libtidy-0.99.so.0.0.0 ${buildroot}${php80_location}/lib
+    cp -a /usr/lib/libtidy.a ${buildroot}${php80_location}/lib
     cp -a /usr/lib/libtidy.so ${buildroot}${php80_location}/lib
+    cp -a /usr/lib/libtidy.la ${buildroot}${php80_location}/lib
     # Fix libreadline
     cp -a /usr/lib/x86_64-linux-gnu/libreadline.a ${buildroot}${php80_location}/lib
-    cp -a /lib/x86_64-linux-gnu/libreadline.so.7 ${buildroot}${php80_location}/lib
-    cp -a /lib/x86_64-linux-gnu/libreadline.so.7.0 ${buildroot}${php80_location}/lib
-    ln -s libreadline.so.7 libreadline.so
+    cp -a /lib/x86_64-linux-gnu/libreadline.so.6 ${buildroot}${php80_location}/lib
+    cp -a /lib/x86_64-linux-gnu/libreadline.so.6.3 ${buildroot}${php80_location}/lib
+    ln -s libreadline.so.6 libreadline.so
     # Fix libvpx
+    cp -a /usr/lib/x86_64-linux-gnu/libvpx.so.1 ${buildroot}${php80_location}/lib
+    cp -a /usr/lib/x86_64-linux-gnu/libvpx.so.1.3.0 ${buildroot}${php80_location}/lib
     cp -a /usr/lib/x86_64-linux-gnu/libvpx.so ${buildroot}${php80_location}/lib
-    cp -a /usr/lib/x86_64-linux-gnu/libvpx.so.5.0.0 ${buildroot}${php80_location}/lib
-    cp -a /usr/lib/x86_64-linux-gnu/libvpx.so.5.0 ${buildroot}${php80_location}/lib
-    cp -a /usr/lib/x86_64-linux-gnu/libvpx.so.5 ${buildroot}${php80_location}/lib
+    cp -a /usr/lib/x86_64-linux-gnu/libvpx.so.1.3 ${buildroot}${php80_location}/lib
     cp -a /usr/lib/x86_64-linux-gnu/libvpx.a ${buildroot}${php80_location}/lib
     # Fix libtinfo
     cp -a /usr/lib/x86_64-linux-gnu/libtinfo.a ${buildroot}${php80_location}/lib
@@ -739,13 +740,43 @@ _build_deb(){
     ln -s libtinfo.so.5 libtinfo.so
     # Fix libzip
     cp -a /usr/lib/x86_64-linux-gnu/libzip.a ${buildroot}${php80_location}/lib
-    cp -a /usr/lib/x86_64-linux-gnu/libzip.so.4.0.0 ${buildroot}${php80_location}/lib
-    ln -s libzip.so.4.0.0 libzip.so.4
-    ln -s libzip.so.4 libzip.so
+    cp -a /usr/lib/x86_64-linux-gnu/libzip.so.2.1.0 ${buildroot}${php80_location}/lib
+    cp -a /usr/lib/x86_64-linux-gnu/libzip.so ${buildroot}${php80_location}/lib
+    cp -a /usr/lib/x86_64-linux-gnu/libzip ${buildroot}${php80_location}/lib
+    cp -a /usr/lib/x86_64-linux-gnu/libzip.so.2 ${buildroot}${php80_location}/lib
+    # Fix libz
+    cp -a /lib/x86_64-linux-gnu/libz.so.1.2.8 ${buildroot}${php80_location}/lib
+    cp -a /usr/lib/x86_64-linux-gnu/libz.a ${buildroot}${php80_location}/lib
+    ln -s libz.so.1.2.8 libz.so.1
+    ln -s libz.so.1 libz.so
     # Fix libonig
+    cp -a /usr/lib/x86_64-linux-gnu/libonig.so.2.0.1 ${buildroot}${php80_location}/lib
     cp -a /usr/lib/x86_64-linux-gnu/libonig.so ${buildroot}${php80_location}/lib
-    cp -a /usr/lib/x86_64-linux-gnu/libonig.so.4 ${buildroot}${php80_location}/lib
-    cp -a /usr/lib/x86_64-linux-gnu/libonig.so.4.0.0 ${buildroot}${php80_location}/lib
+    cp -a /usr/lib/x86_64-linux-gnu/libonig.so.2 ${buildroot}${php80_location}/lib
+    # Fix libpng
+    cp -a /usr/lib/x86_64-linux-gnu/libpng12.so.0 ${buildroot}${php80_location}/lib
+    cp -a /usr/lib/x86_64-linux-gnu/libpng.a ${buildroot}${php80_location}/lib
+    cp -a /usr/lib/x86_64-linux-gnu/libpng.so ${buildroot}${php80_location}/lib
+    cp -a /usr/lib/x86_64-linux-gnu/libpng12.so ${buildroot}${php80_location}/lib
+    cp -a /usr/lib/x86_64-linux-gnu/libpng12.a ${buildroot}${php80_location}/lib
+    # Fix libjpeg
+    cp -a /usr/lib/x86_64-linux-gnu/libjpeg.a ${buildroot}${php80_location}/lib
+    cp -a /usr/lib/x86_64-linux-gnu/libjpeg.so ${buildroot}${php80_location}/lib
+    cp -a /usr/lib/x86_64-linux-gnu/libjpeg.so.62.1.0 ${buildroot}${php80_location}/lib
+    cp -a /usr/lib/x86_64-linux-gnu/libjpeg.so.62 ${buildroot}${php80_location}/lib
+    # Fix libwebp
+    cp -a /usr/lib/x86_64-linux-gnu/libwebpdemux.so.1.0.1 ${buildroot}${php80_location}/lib
+    cp -a /usr/lib/x86_64-linux-gnu/libwebp.so.5 ${buildroot}${php80_location}/lib
+    cp -a /usr/lib/x86_64-linux-gnu/libwebp.so ${buildroot}${php80_location}/lib
+    cp -a /usr/lib/x86_64-linux-gnu/libwebpmux.so ${buildroot}${php80_location}/lib
+    cp -a /usr/lib/x86_64-linux-gnu/libwebp.a ${buildroot}${php80_location}/lib
+    cp -a /usr/lib/x86_64-linux-gnu/libwebpmux.so.1 ${buildroot}${php80_location}/lib
+    cp -a /usr/lib/x86_64-linux-gnu/libwebp.so.5.0.1 ${buildroot}${php80_location}/lib
+    cp -a /usr/lib/x86_64-linux-gnu/libwebpdemux.so.1 ${buildroot}${php80_location}/lib
+    cp -a /usr/lib/x86_64-linux-gnu/libwebpmux.so.1.0.1 ${buildroot}${php80_location}/lib
+    cp -a /usr/lib/x86_64-linux-gnu/libwebpmux.a ${buildroot}${php80_location}/lib
+    cp -a /usr/lib/x86_64-linux-gnu/libwebpdemux.so ${buildroot}${php80_location}/lib
+    cp -a /usr/lib/x86_64-linux-gnu/libwebpdemux.a ${buildroot}${php80_location}/lib
     echo "${php80_location}/lib" > ${buildroot}/etc/ld.so.conf.d/php80.conf
 
     cat > ${buildroot}/DEBIAN/control << EOF
